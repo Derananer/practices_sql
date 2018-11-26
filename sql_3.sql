7fb06cf9f48fd77ec0909d5b097270c84e34356b
Derananer <10ilay1998@gmail.com>
2018-11-26 20:17:28 +0400

    finish sql_3.sql

--- sql_3.sql
+++ sql_3.sql
@@ -19,6 +19,8 @@ SELECT ENAME, DEPTNO, SAL, COMM FROM EMP EMP1 WHERE DEPTNO = ANY(SELECT DEPTNO F
 работников, чьи номер подразделения и зарплата совпадают (одновременно) с номером
 подразделения и зарплатой любого из работников, получающих комиссионные.*/
 
+--НЕ РОБИТ
+SELECT ENAME, DEPTNO, SAL, COMM FROM EMP EMP1 WHERE ((SAL,DEPTNO) IN (SELECT SAL, DEPTNO FROM EMP EMP2  WHERE EMP1.EMPNO<>EMP2.EMPNO));
 SELECT EMPNO, ENAME FROM EMP WHERE SAL>(SELECT MAX(SAL) FROM EMP E WHERE E.JOB = 'CLERK');/*9)Сформировать запрос, выводящий номера и имена работников, зарплата которых больше
 зарплаты любого из клерков.*/
 
@@ -26,3 +28,29 @@ SELECT EMPNO, ENAME FROM EMP WHERE SAL>(SELECT MAX(SAL) FROM EMP E WHERE E.JOB =
 SELECT EMPNO, ENAME FROM EMP WHERE SAL > ALL(SELECT SAL FROM EMP E WHERE E.JOB = 'CLERK');/*9)Сформировать запрос, выводящий номера и имена работников, зарплата которых больше
 зарплаты любого из клерков.*/
 
+SELECT COUNT(*) FROM EMP WHERE DEPTNO = (SELECT DEPTNO FROM DEPT WHERE LOC = 'DALLAS');/*10)Сформировать запрос, выводящий количество людей, работающих в Dallas.*/
+
+SELECT COUNT(*) FROM EMP WHERE JOB = 'MANAGER';/*11)Сформировать запрос, подсчитывающий общее число менеджеров.*/
+
+SELECT COUNT(*) FROM EMP WHERE EMPNO = ANY(SELECT MGR FROM EMP);/*11)Сформировать запрос, подсчитывающий общее число менеджеров.*/
+
+--FIGNYA
+SELECT lpad(' ', 4*LEVEL) || ENAME || ' ' || LEVEL || '/' AS TREE FROM EMP CONNECT BY PRIOR MGR = EMPNO ;/*12)Сформировать запрос, выводящий фамилию сотрудника, номер узла иерархии и имена
+всех его менеджеров через /.*/
+
+SELECT ENAME , LEVEL, SYS_CONNECT_BY_PATH(ENAME,'/') FROM EMP START WITH MGR IS NULL CONNECT BY PRIOR EMPNO = MGR;/*12)Сформировать запрос, выводящий фамилию сотрудника, номер узла иерархии и имена
+всех его менеджеров через /.*/
+
+SELECT EMP.ENAME, EMP.EMPNO, DEPT.DNAME,LEVEL, SYS_CONNECT_BY_PATH(ENAME,'/') FROM EMP,DEPT WHERE EMP.DEPTNO = DEPT.DEPTNO START WITH MGR IS NULL CONNECT BY PRIOR EMPNO=MGR;/*13)Сформировать запрос, выводящий фамилию сотрудника, номер и название
+подразделения, где он работает, номер узла иерархии и имена всех его менеджеров через /.*/
+
+SELECT EMP.ENAME, EMP.EMPNO, DEPT.DNAME,LEVEL, SYS_CONNECT_BY_PATH(ENAME,'/') FROM EMP,DEPT WHERE EMP.DEPTNO = DEPT.DEPTNO START WITH MGR IS NULL CONNECT BY PRIOR EMPNO=MGR ORDER SIBLINGS BY DEPT.DNAME ;/*14)Сформировать запрос, выводящий фамилию сотрудника, номер и название
+подразделения, где он работает, номер узла иерархии и имена всех его менеджеров через /.
+Внутри одного уровня иерархии сотрудники должны быть отсортированы по названиям
+подразделения.*/
+
+SELECT EMP.ENAME, E.MSYSPATH FROM (SELECT EMPNO,SYS_CONNECT_BY_PATH(ENAME,'/') AS MSYSPATH FROM EMP START WITH MGR IS NULL CONNECT BY PRIOR EMPNO= MGR ) E , EMP WHERE EMP.MGR = E.EMPNO ;/*15)Сформировать запрос, выводящий фамилию сотрудника, номер узла иерархии и имена
+всех его менеджеров через /, исключая фамилию самого сотрудника.*/
+
+SELECT * FROM EMP;
+SELECT * FROM DEPT;
